7af8f62bc2408bdec162b31d047f18607d93729f
William.Aroche21 <williamaroche1234@gmail.com>
2024-03-08 17:26:32 -0600

    configuraciones yml y astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,14 @@ import svelte from "@astrojs/svelte";
 
 import netlify from "@astrojs/netlify";
 
-const SERVER_PORT = 3000;
-const LOCALHOST_URL = 'http://localhost:'+ SERVER_PORT;
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const LIVE_URL = "https://willking20.github.io";
-const isBuild = SCRIPT.includes("astro build");
-const BASE_URL = LOCALHOST_URL;
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
-
 // https://astro.build/config
 export default defineConfig({
+  server: 3000,
+  site: "https://willking20.github.io",
   integrations: [
     tailwind(), 
     svelte()],
   output: "server",
   adapter: netlify(),
-  site: BASE_URL,
-  base: '/Myndverse',
+  base: "/Myndverse/"
 });
